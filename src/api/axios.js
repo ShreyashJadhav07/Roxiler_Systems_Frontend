79bffcd8a581b3c64aa6a32f72648103cfc50609
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-22 12:49:01 +0530

    axios url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,20 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//     baseURL: "http://localhost:3000",
+//     withCredentials: true,
+
+
+// })
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true,
-
-
-})
+  baseURL: "https://roxiler-systems-submission-g7p9.onrender.com",
+  withCredentials: true,
+   
+});
 
 export default axiosInstance;
